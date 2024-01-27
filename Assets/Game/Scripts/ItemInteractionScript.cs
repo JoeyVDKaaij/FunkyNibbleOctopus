a049f3be7472da41cb5ae52076b959ce3860b989
Joey van der Kaaij <joey.vanderkaaij@gmail.com>
2024-01-27 12:44:37 +0100

    Improved pickupscript
    Pick up and drop now also works with E instead of just the a button on xbox controller. The order now always gets on the table

--- Assets/Game/Scripts/ItemInteractionScript.cs
+++ Assets/Game/Scripts/ItemInteractionScript.cs
@@ -7,32 +7,41 @@ public class ItemInteractionScript : MonoBehaviour
     
     void Update()
     {
-        if (Input.GetKey(KeyCode.JoystickButton0) && _childObject != null && !_buttonPressed)
+        if (_childObject != null && !_buttonPressed)
         {
-            _childObject.localPosition = new Vector3(0, 0, 1);
-            _childObject.SetParent(null);
-            _childObject = null;
-            _buttonPressed = true;
+            if (Input.GetKey(KeyCode.JoystickButton0) || Input.GetKey(KeyCode.E))
+            {
+                _childObject.localPosition = new Vector3(0, 0, 1);
+                _childObject.SetParent(null);
+                _childObject = null;
+                _buttonPressed = true;
+            }
         }
 
-        if (Input.GetKeyUp(KeyCode.JoystickButton0)) _buttonPressed = false;
+        if (Input.GetKeyUp(KeyCode.JoystickButton0) || Input.GetKeyUp(KeyCode.E)) _buttonPressed = false;
     }
 
     private void OnCollisionStay(Collision other)
     {
-        if (Input.GetKey(KeyCode.JoystickButton0) && other.gameObject.CompareTag("Dish") && transform.childCount <= 1 && !_buttonPressed && !(other.transform.parent != null))
+        if (other.gameObject.CompareTag("Dish") && transform.childCount <= 1 && !_buttonPressed && !(other.transform.parent != null))
         {
-            other.transform.SetParent(transform);
-            _childObject = other.transform;
-            _childObject.localPosition = new Vector3(0, 1, 0);
-            _buttonPressed = true;
+            if (Input.GetKey(KeyCode.JoystickButton0) || Input.GetKey(KeyCode.E))
+            {
+                other.transform.SetParent(transform);
+                _childObject = other.transform;
+                _childObject.localPosition = new Vector3(0, 1.2f, 0);
+                _buttonPressed = true;
+            }
         }
-        else if (Input.GetKey(KeyCode.JoystickButton0) && other.gameObject.CompareTag("Table") && _childObject != null && !_buttonPressed)
+        else if (other.gameObject.CompareTag("Table") && _childObject != null && !_buttonPressed)
         {
-            _childObject.SetParent(other.transform);
-            _childObject.localPosition = new Vector3(0, 1, 0);
-            _childObject = null;
-            _buttonPressed = true;
+            if (Input.GetKey(KeyCode.JoystickButton0) || Input.GetKey(KeyCode.E))
+            {
+                _childObject.SetParent(other.transform);
+                _childObject.localPosition = new Vector3(0, 1, 0);
+                _childObject = null;
+                _buttonPressed = true;
+            }
         }
     }
 }
