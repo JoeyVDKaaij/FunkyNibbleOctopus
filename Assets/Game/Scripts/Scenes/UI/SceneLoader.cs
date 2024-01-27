911e3fca93ef52216f1b3180fa5935a6ea00b447
André Santiago <andre.mpc.santiago@gmail.com>
2024-01-27 04:04:02 +0100

    feat(scene.mainmenu): add base main menu scene

---
+++ Assets/Game/Scripts/Scenes/UI/SceneLoader.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace Game.Scenes.UI
+{
+    [RequireComponent(typeof(Button))]
+    public class SceneLoader : MonoBehaviour
+    {
+        [SerializeField]
+        private SceneContainer targetScene;
+
+        [Inject]
+        private ScenesController _scenesController;
+
+        private Button _button;
+
+        private void Awake ()
+        {
+            _button = GetComponent<Button>();
+        }
+
+        private void OnEnable ()
+        {
+            _button.onClick.AddListener(LoadScene);
+        }
+
+        private void OnDisable ()
+        {
+            _button.onClick.RemoveListener(LoadScene);
+        }
+
+        public void LoadScene ()
+        {
+            _scenesController.LoadScene(targetScene).Forget();
+        }
+    }
+}
