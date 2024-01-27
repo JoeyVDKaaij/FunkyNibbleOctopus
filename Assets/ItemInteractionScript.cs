2ce2f204b0ef4342389eceb281684fd71bab82c3
Joey van der Kaaij <joey.vanderkaaij@gmail.com>
2024-01-27 07:09:20 +0100

    Pickup and drop mechanic finished

--- Assets/ItemInteractionScript.cs
+++ Assets/ItemInteractionScript.cs
@@ -1,27 +1,31 @@
-using System;
 using UnityEngine;
 
 public class ItemInteractionScript : MonoBehaviour
 {
-    private Transform childObject;
+    private Transform _childObject;
+    private bool _buttonPressed;
     
     void Update()
     {
-        if (Input.GetKey(KeyCode.JoystickButton0) && childObject != null)
+        if (Input.GetKey(KeyCode.JoystickButton0) && _childObject != null && !_buttonPressed)
         {
-            childObject.localPosition = new Vector3(0, 0, 1);
-            childObject.SetParent(null);
-            childObject = null;
+            _childObject.localPosition = new Vector3(0, 0, 1);
+            _childObject.SetParent(null);
+            _childObject = null;
+            _buttonPressed = true;
         }
+
+        if (Input.GetKeyUp(KeyCode.JoystickButton0)) _buttonPressed = false;
     }
 
     private void OnCollisionStay(Collision other)
     {
-        if (Input.GetKeyDown(KeyCode.JoystickButton0) && other.gameObject.CompareTag("Dish") && transform.childCount <= 1)
+        if (Input.GetKey(KeyCode.JoystickButton0) && other.gameObject.CompareTag("Dish") && transform.childCount <= 1 && !_buttonPressed)
         {
             other.transform.SetParent(transform);
-            childObject = other.transform;
-            childObject.localPosition = new Vector3(0, 1, 0);
+            _childObject = other.transform;
+            _childObject.localPosition = new Vector3(0, 1, 0);
+            _buttonPressed = true;
         }
     }
 }
